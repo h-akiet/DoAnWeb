b3187f1fa2e2a8ec9d297030d47786cbe774b127
trungtran1042005 <trungtran1042005@gmail.com>
2025-10-13 21:51:35 +0700

    Thêm OneShop vào nhành

---
+++ OneShop/src/main/java/com/oneshop/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.oneshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "guest/index";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "guest/cart";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "guest/contact";
+    }
+
+    @GetMapping("/list-product")
+    public String listProduct() {
+        return "guest/listProduct";
+    }
+
+    @GetMapping("/news")
+    public String news() {
+        return "guest/news";
+    }
+
+    @GetMapping("/pay")
+    public String pay() {
+        return "guest/pay";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "guest/product";
+    }
+}
